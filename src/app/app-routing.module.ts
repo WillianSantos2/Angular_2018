c0716af93c510504e3810dac78f34562678bbdad
Aluno <fabianomoreira.prof@gmail.com>
2019-04-17 11:50:42 -0300

    17042019

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddComponent } from './cliente/add/add.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "cadastro", component: AddComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './page/nav/nav.component';
+import { FooterComponent } from './page/footer/footer.component';
+import { MainComponent } from './page/main/main.component';
+import { AddComponent } from './cliente/add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    MainComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
